b8abca68d7b949e07160a9bf42842c785e108669
sebastianmayorga <j_25005@hotmail.com>
2019-05-16 13:12:38 -0500

    Commplement Comprueba si el tipo de carta del

--- Game.rb
+++ Game.rb
@@ -33,6 +33,7 @@ class Game
   end
 
   # create_player: Agrega un jugador a la lista de jugadores
+  #
   # name: nombre del jugador
   def create_player(name)
     @players.push(Player.new(name, name, 0))
@@ -50,7 +51,7 @@ class Game
       end
     end
   end
-
+  # Muestra las cartas de cada jugador
   def show_players
     puts @cards.length
     @players.each do |player|
@@ -61,9 +62,13 @@ class Game
     end
   end
 
+  # Selecciona quien inicia la partida y luego hace un ciclo hasta que los jugadores se queden sin cartas
+  # En el ciclo se muestra la ronda que se esta jugando
+  # llama a ronda
+  # luego llama whoWinRound()
+  # Y muestra quien gano la ronda con la carta que gano
+  # Al terminar el ciclo, llama a who_win
   def start_game
-    start = true
-    cant_players = @players.length
     player_random = select_player_random
     cont = 1
     until @players[0].cards.empty?
@@ -80,6 +85,14 @@ class Game
 
   end
 
+  # who_win: Recorre el array de players y dentro,
+  # recorre el array de cartas ganadas de cada jugador
+  # haciendo la suma de las cartas ganadas guardando el resultado
+  # en la variable de poinst de cada jugador,
+  # Una vez finalizado ordena el array de players de mayor
+  # a menor con la variable poinst, se selecciona el primer player
+  # y se muestran los datos del nombre y los puntos del jugador ganador
+  # despues se listan los jugadores con sus cartas
   def who_win
     @players.each do |player|
       result_sum = 0
@@ -102,6 +115,11 @@ class Game
     end
   end
 
+  # save_card_into_player_win: Guarda las cartas que estan en la mesa
+  # en el array de cartas ganadas que tiene el player,
+  # y las va elminando de la lista de cartas de la mesa.
+  #
+  # player: Jugador que gano la ronda
   def save_card_into_player_win(player)
     cont = @cards_thrown.length - 1
     while cont >= 0
@@ -112,6 +130,25 @@ class Game
     end
   end
 
+  # whoWinRound: Toma la carta del player que empezo la ronda
+  # luego hace un ciclo recorriendo los players.
+  # En el ciclo toma la carta del player,
+  # valida que si la carta del player que inicio la ronda no es la misma
+  # que la del player a comprobar.
+  # Si no son las mismas, valida que el tipo de la carta del player sea igual
+  # al tipo de la carta del triunfo o que el tipo de la carta del player que
+  # inicio la ronda sea igual al del triunfo.
+  # Si alguno de los dos es valido va a validate_card_with_trump y
+  # guarda el resultado.
+  # Si no va a validate_card_without_trump y guarda el resultado
+  # Luego comprueba que save_card_win no sea nulo
+  # Si no es nulo guarda el resultado.
+  #
+  # Luego recorre la lista de players y comprueba si la carta que lanzo el
+  # jugador es igual a la carta que se selecciona como ganadora retorna la
+  # posicion del player
+  #
+  # num_player_start: posicion del player que inicio la ronda
   def whoWinRound(num_player_start)
     cont = 0
     card_win = @players[num_player_start].card_thrown
@@ -137,7 +174,10 @@ class Game
     end
   end
 
-  # round: recorre la lista de jugadores
+  # round: recorre la lista de players desde el jugador que inicio la ronda
+  # hasta que llega a una posicion antes de el.
+  # Si hay cartas en la mesa, las muestra por cada jugador
+  #
   # num_player: numero que selecciona que jugador inicia la ronda
   def round(num_player)
     cont = 1
@@ -156,8 +196,10 @@ class Game
     end
   end
 
-  # throw_card_player: Muestra las cartas del jugador y pide que seleccione una para lanzar
-  # player: Es el jugador que va a lanzar
+  # throw_card_player: Muestra las cartas del player y va a get_card,
+  # si el resultado no es valido vuelve a get_card hasta que sea valido
+  #
+  # player: Es el player que va a lanzar
   def throw_card_player(player)
     puts "Carta del triunfo: Numero: #{@trump_card.number}, Tipo: #{@trump_card.type}"
     puts "Turno para el jugador: #{player.name}"
@@ -171,9 +213,10 @@ class Game
     end
   end
 
-  # search_card: Busca la carta en las cartas del jugador, Si la encuentra, retorna la carta,
-  # si no, retorna null
-  # player: Es el jugador
+  # search_card: Busca la carta en las cartas del player, Si la encuentra,
+  # retorna la carta, si no, retorna null
+  #
+  # player: Es el player
   # id_card: El id de la carta seleccionada
   def search_card(player, id_card)
     player.cards.each do |card|
@@ -182,7 +225,7 @@ class Game
     NIL
   end
 
-  # select_player_random: Selecciona aleatoriamente un jugador y lo retorna
+  # select_player_random: Selecciona aleatoriamente un player y lo retorna
   def select_player_random
     num_player = rand(@players.length)
     num_player
@@ -190,6 +233,8 @@ class Game
 
   private
 
+  # save_card_win: Si la carta que recive no es unal guarda la carta y la retorna
+  # aux_card: Carta a validar
   def save_card_win(aux_card)
     if aux_card.nil?
     else
@@ -198,15 +243,24 @@ class Game
     card_win
   end
 
-  # validate_card_with_trump: Valida si la carta es la posible gandaora con el triunfo
-  # card: carta posible ganadora
-  # card_win: La carta que va ganando
+  # validate_card_with_trump: Comprueba si el tipo de carta del
+  # player a comprobar es igual al tipo de carta del triunfo Y
+  # si el tipo de carta del player que inicio es diferente al tipo
+  # de carta del triunfo, retorna la carta del player a comprobar.
+  # Si no.
+  # Comprueba si el tipo de carta del player que inicio es igual al tipo
+  # de carta del triunfo Y si el tipo de carta del player a comprobar es
+  # diferente al tipo de carta del triunfo,
+  # retorna la carta del player que inicio
+  #
+  # card: La carta del player a comprobar
+  # card_win: La carta del player que inicio la ronda
   def validate_card_with_trump(card, card_win)
     if card.type.to_s == @trump_card.type.to_s &&
-        card_win.type.to_s != @trump_card.type.to_s
+       card_win.type.to_s != @trump_card.type.to_s
       return card
     elsif card_win.type.to_s == @trump_card.type.to_s &&
-        card.type.to_s != @trump_card.type.to_s
+          card.type.to_s != @trump_card.type.to_s
       return card_win
     end
 
@@ -218,9 +272,18 @@ class Game
     NIL
   end
 
-  # validate_card_without_trump: Valida si la carta es la posible gandaora sin el triunfo
-  # card: carta posible ganadora
-  # card_win: La carta que va ganando
+  # validate_card_without_trump: Comprueba si el tipo de la carta del player
+  # que lanzo de primeras, es igual a la del player a comprobar,
+  # y si (El valor de la carta del player que inicio es 0 o
+  # El valor de la carta del player a comprobar es 0),
+  # devuelve la carta del player a comprobar si el valor de la carta
+  # es mayor al del player que lanzo primero.
+  # Si no devuelve la carta del player a comprobar si el numero
+  # de la carta es mayor.
+  # Si el tipo de carta no es igual devuelve null.
+  #
+  # card: La carta del player a comprobar
+  # card_win: La carta del player que inicio la ronda
   def validate_card_without_trump(card, card_win)
     if card_win.type.to_s == card.type.to_s
       if !card_win.value.to_i.zero? || !card.value.to_i.zero?
@@ -232,8 +295,14 @@ class Game
     NIL
   end
 
-  # get_card: Pedir el id de la carta a lanzar por teclado y validarla
-  # player: El jugador
+  # get_card: Pide el id de la carta a lanzar por teclado,
+  # luego va a search_card y si no retorna null guarda la carta en el player
+  # en la variable cart_thrown (carta lanzada) luego la guarda en cards_trhown
+  # (lista de cartas en la mesa) y luego la elimina de la lista de cartas del
+  # player.
+  # Si retorna null muestra el mensaje "Id no valido"
+  #
+  # player: El player que tiene que lanzar la carta
   def get_card(player, card_valid)
     puts 'Digite el id de la carta a lanzar'
     card_id = gets.chomp.to_s
@@ -252,6 +321,8 @@ class Game
   end
 
   # save_card: Se guardan las cartas en el array cards
+  # card: json de la carta
+  # type: pala de la carta
   def save_card(card, type)
     card.each do |i|
       @cards.push(Card.new((i['number'].to_s + type.to_s), i['number'].to_s,
